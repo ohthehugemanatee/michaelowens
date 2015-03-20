48598d1ea99d8d13302b970869d29939d860f34e
Campbell Vertesi <campbell@vertesi.com>
2015-03-20 19:18:26 +0100

    try to re-order featured items title/image with JS in functions.php

--- public/wp-content/themes/customizr-child/functions.php
+++ public/wp-content/themes/customizr-child/functions.php
@@ -24,3 +24,46 @@ add_shortcode('htmlify', 'do_html');
 function do_html($attr, $content){
       return str_replace(array('{','}'), array('<','>'), $content);
 }
+
+
+add_action('__after_footer' , 'set_fp_item_order');
+function set_fp_item_order() {
+  $my_item_order = array(
+    'title',
+    'image',
+    'text',
+    'button',
+  );
+  ?>
+  <script type="text/javascript">
+    jQuery(document).ready(function () {
+      ! function ($) {
+        //prevents js conflicts
+        "use strict";
+        var my_item_order 	= [<?php echo '"'.implode('","', $my_item_order).'"' ?>],
+          $Wrapper 		= '';
+
+        if ( 0 != $('.widget-front' , '#main-wrapper .marketing' ).length ) {
+          $Wrapper = $('.widget-front' , '#main-wrapper .marketing' );
+        } else if ( 0 != $('.fpc-widget-front' , '#main-wrapper .fpc-marketing' ).length ) {
+          //for FPU users
+          $Wrapper = $('.fpc-widget-front' , '#main-wrapper .fpc-marketing' );
+        } else {
+          return;
+        }
+
+        $Wrapper.each( function() {
+          var o            = [];
+          o['title']   = $(this).find('h2');
+          o['image']   = $(this).find('.thumb-wrapper');
+          o['text']    = $(this).find('p');
+          o['button']  = $(this).find('a.btn');
+          for (var i = 0; i < my_item_order.length - 1; i++) {
+            o[my_item_order[i]].after(o[my_item_order[i+1]]);
+          };
+        });
+      }(window.jQuery)
+    });
+  </script>
+<?php
+}
